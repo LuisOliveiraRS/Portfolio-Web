e709cf879a543e93d9f71aa49e7e7693c9b0137d
LuisOliveiraRS <99623002+LuisOliveiraRS@users.noreply.github.com>
2025-09-09 23:33:26 -0300

    wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'luisoliveiracom_wp379' );
+define( 'DB_NAME', 'xxxxxxxxx' );
 
 /** Database username */
-define( 'DB_USER', 'luisoliveiracom_wp379' );
+define( 'DB_USER', 'xxxxxxxxxx' );
 
 /** Database password */
-define( 'DB_PASSWORD', '.Syu(00p3t' );
+define( 'DB_PASSWORD', 'xxxxxxx' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
